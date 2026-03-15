4a836927463543f5f9d43192ccc0015e692a78ec
Phan Long <phanmainhatlong@outlook.com>
2026-03-15 23:07:25 +0700

    Migration: user & session table

--- migration/src/m20260305_081157_login_signup_migration.rs
+++ migration/src/m20260305_081157_login_signup_migration.rs
@@ -9,27 +9,138 @@ impl MigrationTrait for Migration {
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(User::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(uuid(User::Id).primary_key())
+                    .col(string(User::FullName))
+                    .col(string(User::Email))
+                    .col(string(User::PasswordHash))
+                    .col(string(User::PhoneNumber))
+                    .col(string(User::AccountStatus))
+                    .col(string(User::RoleID))
+                    .col(timestamp_null(CreatedAt))
+                    .col(timestamp_null(UpdatedAt))
+                    .col(timestamp_null(DeletedAt))
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk_user_account_status")
+                            .from(User::Table, User::AccountStatus)
+                            .to(AccountStatus::Table, AccountStatus::Id)
+                            .on_delete(ForeignKeyAction::Restrict)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk_user_role_id")
+                            .from(User::Table, User::RoleID)
+                            .to(Role::Table, Role::Id)
+                            .on_delete(ForeignKeyAction::Restrict)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
                     .to_owned(),
             )
-            .await
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Role::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Role::Id))
+                    .col(string(Role::Name))
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(AccountStatus::Table)
+                    .if_not_exists()
+                    .col(pk_auto(AccountStatus::Id))
+                    .col(string(AccountStatus::Name))
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Session::Table)
+                    .if_not_exists()
+                    .col(uuid(Session::Id).primary_key())
+                    .col(uuid(Session::UserID))
+                    .col(string(Session::AccessToken))
+                    .col(string(Session::DeviceFingerprint))
+                    .col(string(Session::IPAddress))
+                    .col(timestamp_null(CreatedAt))
+                    .col(timestamp_null(Session::ExpiresAt))
+                    .col(timestamp_null(Session::RevokedAt))
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk_session_user_id")
+                            .from(Session::Table, Session::UserID)
+                            .to(User::Table, User::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(User::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+struct CreatedAt;
+
+#[derive(DeriveIden)]
+struct UpdatedAt;
+
+#[derive(DeriveIden)]
+struct DeletedAt;
+
+#[derive(DeriveIden)]
+enum User {
     Table,
     Id,
-    Title,
-    Text,
+    FullName,
+    Email,
+    PasswordHash,
+    PhoneNumber,
+    AccountStatus,
+    RoleID,
+}
+
+#[derive(DeriveIden)]
+enum Role {
+    Table,
+    Id,
+    Name,
+}
+
+#[derive(DeriveIden)]
+enum AccountStatus {
+    Table,
+    Id,
+    Name,
+}
+
+#[derive(DeriveIden)]
+enum Session {
+    Table,
+    Id, // also refresh token
+    UserID,
+    AccessToken,
+    DeviceFingerprint,
+    IPAddress,
+    ExpiresAt,
+    RevokedAt,
 }
