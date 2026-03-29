f98c4f9a952510764d400a1580ae32d3e4c7f4b2
Andexenn <phanhuynhhung0707@gmail.com>
2026-03-30 01:11:12 +0700

    Feat(migration): Device migration file

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,7 +1,8 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20260305_081157_login_signup_migration;
-mod m20260318_051423_work_orders;
+mod m20260318_051423_work_orders;mod m20260329_174031_device;
+
 
 pub struct Migrator;
 
